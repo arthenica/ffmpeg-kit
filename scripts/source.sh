a876992f90d2ca46de19aa697bd5a48fdb0fcd29
Taner Sener <tanersener@gmail.com>
2022-10-22 21:34:49 +0100

    use the latest ffmpeg commit as of today to build ffmpeg

--- scripts/source.sh
+++ scripts/source.sh
@@ -33,8 +33,8 @@ get_library_source() {
     ;;
   ffmpeg)
     SOURCE_REPO_URL="https://github.com/arthenica/FFmpeg"
-    SOURCE_ID="n5.1.2"
-    SOURCE_TYPE="TAG"
+    SOURCE_ID="65f96a965af415300f7533c75940eeae4e3a2287"
+    SOURCE_TYPE="COMMIT"
     ;;
   fontconfig)
     SOURCE_REPO_URL="https://github.com/arthenica/fontconfig"
